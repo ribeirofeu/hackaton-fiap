93287e2fe1b78ed8c85a645752592ac170cc861c
Felipe Ribeiro <ribeiro.feu@gmail.com>
2024-03-18 23:08:52 -0300

    commit inicial

---
+++ src/main/java/com/hackaton/hackton/HacktonApplication.java
@@ -0,0 +1,13 @@
+package com.hackaton.hackton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HacktonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HacktonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackaton/hackton/HacktonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackaton.hackton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HacktonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
